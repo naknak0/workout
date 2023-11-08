827c22b0d9b61355506238fe34e1d6e2684ad0d7
naknvn77@gmail.com <naknvn77@gmail.com>
2023-11-09 00:12:50 +0900

    jQuery

---
+++ 01/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("*").css("border", "1px solid #00f");
+});
+
+$(function () {
+  $("h1").css("background", "pink");
+  $("h2 p").css("background", "yellow");
+  $("p").next().css("color", "#fff");
+  $("h2").next().css("background", "#f00");
+});
