465c5b189871adf329ab436e16419b6099221703
naknvn77@gmail.com <naknvn77@gmail.com>
2023-11-15 23:20:13 +0900

    fullpage

--- 01/script.js
+++ 01/script.js
@@ -1,27 +1,48 @@
-$(function () {
-  /*
-  $("li:first-of-type").css({
-    background: "#ff0",
-  });
-  $("li:last-of-type").css({
-    background: "#0ff",
-  });
-  */
-  /*
-  $("#menu1 li:nth-child(1)").css({
-    background: "#ff0",
-  });
-  $("#menu1 li:nth-child(2n)").css({
-    border: "2px dashed #f00",
-  });
-  $("#menu2 li:nth-last-child(2)").css({
-    background: "#0ff",
-  });
-  */
-  $("#menu1 li").slice(1, 3).css({
-    background: "#ff0",
-  });
-  $("li:only-child").css({
-    background: "#0ff",
-  });
+const Body = document.querySelector("body");
+const Nav_btn = document.querySelector("#nav_icon");
+const sec2_Title = document.querySelector("#sec2 h1.title");
+const sec2_Slider = document.querySelector("#sec2 .slider_wrap");
+
+Nav_btn.addEventListener("click", () => {
+  Body.classList.toggle("nav_active");
+});
+
+const sec2 = () => {
+  sec2_Title.style.cssText = `
+  opacity: 1;
+  transform: translateX(50px);
+  `;
+  sec2_Slider.style.cssText = `
+  opacity: 1;
+  transform: translateX(-50px);
+  `;
+};
+
+const sec2_reset = () => {
+  sec2_Title.style.cssText = `
+  opacity: 0;
+  transform: translateX(-50px);
+  `;
+  sec2_Slider.style.cssText = `
+  opacity: 0;
+  transform: translateX(50px);
+  `;
+};
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  navigation: true,
+  anchors: ["Num0", "Num1", "Num2", "Num3", "Num4"],
+  afterLoad: (old_elem, new_elem, direction) => {
+    if (new_elem.index === 2) {
+      sec2();
+      console.log("section 2 Hello!");
+    }
+    if (old_elem.index === 2) {
+      sec2_reset();
+      console.log("section 2 good bye!");
+    }
+  },
 });
