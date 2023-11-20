5bc53a6242487f892be8c079c82c26307980c585
naknak0 <naknvn77@gmail.com>
2023-11-20 15:54:59 +0900

    고래

--- 01/script.js
+++ 01/script.js
@@ -1,48 +1,11 @@
-const Body = document.querySelector("body");
-const Nav_btn = document.querySelector("#nav_icon");
-const sec2_Title = document.querySelector("#sec2 h1.title");
-const sec2_Slider = document.querySelector("#sec2 .slider_wrap");
+const path = anime.path(".svg path"); // .path경로를 줄것 svg안에 있는 path경로
 
-Nav_btn.addEventListener("click", () => {
-  Body.classList.toggle("nav_active");
-});
-
-const sec2 = () => {
-  sec2_Title.style.cssText = `
-  opacity: 1;
-  transform: translateX(50px);
-  `;
-  sec2_Slider.style.cssText = `
-  opacity: 1;
-  transform: translateX(-50px);
-  `;
-};
-
-const sec2_reset = () => {
-  sec2_Title.style.cssText = `
-  opacity: 0;
-  transform: translateX(-50px);
-  `;
-  sec2_Slider.style.cssText = `
-  opacity: 0;
-  transform: translateX(50px);
-  `;
-};
-
-new fullpage("#fullpage", {
-  //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
-  navigation: true,
-  anchors: ["Num0", "Num1", "Num2", "Num3", "Num4"],
-  afterLoad: (old_elem, new_elem, direction) => {
-    if (new_elem.index === 2) {
-      sec2();
-      console.log("section 2 Hello!");
-    }
-    if (old_elem.index === 2) {
-      sec2_reset();
-      console.log("section 2 good bye!");
-    }
-  },
+anime({
+  targets: ".circle", //회전하는 div 이름
+  translateX: path("x"),
+  translateY: path("y"),
+  rotate: path("angle"),
+  easing: "linear",
+  duration: 10000, //10초간 천천히 움직여~
+  loop: true,
 });
